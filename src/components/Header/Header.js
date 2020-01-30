61d102a6823ef1fe606429c16ee08bad13df54ac
Алексей Бельченков <u608110@gmail.com>
2020-01-30 18:51:46 +0400

    Reusing components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
+import logo from '../../GitHub-Mark-Light-64px.png';
 import Link from "../Link/Link";
 import './Header.css';
 
-const Header = ({ logo }) => (
+const Header = () => (
     <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link url={'https://reactjs.org'} title={'Learn React'} />
+        <h1>
+            My Github Portfolio
+        </h1>
     </header>
 );
 export default Header;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Header from "../components/Header/Header";
 import './App.css';
 import logo from '../logo.svg';
+import Profile from "./Profile";
 
-function App() {
-  return (
-    <div className="App">
-      <Header logo={logo} />
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header logo={logo} />
+                <Profile />
+            </div>
+        );
+    }
 }
 
 export default App;
